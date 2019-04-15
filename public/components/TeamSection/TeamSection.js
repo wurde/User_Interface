2faf3693d865e96ea9be720b999ed77b675da840
Andy Bettisworth <andy@accreu.com>
2019-04-15 17:52:17 -0500

    Begin implementation of TeamSection component

--- public/components/TeamSection/TeamSection.js
+++ public/components/TeamSection/TeamSection.js
@@ -1,12 +1,6 @@
 'use strict'
 
 /**
- * Dependencies
- */
-
-const TeamCard = require("../TeamCard/TeamCard")
-
-/**
  * Data
  */
 
@@ -23,18 +17,51 @@ const data = [
 ]
 
 /**
- * Define component
+ * Define components
  */
 
 class TeamSection {
   constructor(element) {
     this.element = element
-    
+
+    let container = document.createElement("div")
+    container.classList.add("container")
+    let row = document.createElement("div")
+    row.classList.add("row")
+    container.appendChild(row)
+    this.element.appendChild(container)
+
+    this.team_cards = data.map(x => {
+      new TeamCard(x, row)
+    })
+  }
+}
+
+class TeamCard {
+  constructor(data, row) {
+    this.data = data
+    this.row = row
+
+    let col = document.createElement("div")
+    col.classList.add("col-4")
+    col.classList.add("p-5")
+    this.row.appendChild(col)
+
+    let card = document.createElement("div")
+    card.classList.add("m-3")
+    card.classList.add("border")
+    col.appendChild(card)
+
+    let h2 = document.createElement("h2")
+    h2.classList.add("m-3")
+    h2.classList.add("border")
+    h2.textContent = this.data.name
+    card.appendChild(h2)
   }
 }
 
 /**
- * Mount component
+ * Mount components
  */
 
 let team_section = new TeamSection(document.getElementById("team-section"))
