029c437233331958da0b4e0f88dea70c36801bb3
Ming Liu <mliu@macpro.home>
2019-04-16 13:19:06 -0400

    import ScrollReveal, add sr function/setups, implement sr for feature section, style change to header, and CTA img

---
+++ public/js/sr-functions.js
@@ -0,0 +1,62 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.leftSlide-1', {
+    origin: 'left',
+    distance: '50px',
+    duration: 500,
+    delay:0,
+    opacity:1,
+    scale: 1,
+    easing:'ease-in'
+});
+
+sr.reveal('.leftSlide-2', {
+    origin: 'left',
+    distance: '50px',
+    duration: 700,
+    delay:0,
+    opacity:1,
+    scale: 1,
+    easing:'ease-in'
+});
+
+sr.reveal('.leftSlide-3', {
+    origin: 'left',
+    distance: '50px',
+    duration: 900,
+    delay:0,
+    opacity:1,
+    scale: 1,
+    easing:'ease-in'
+});
+
+sr.reveal('.rightSlide-1', {
+    origin: 'right',
+    distance: '50px',
+    duration: 500,
+    delay:0,
+    opacity:1,
+    scale: 1,
+    easing:'ease-in'
+})
+
+sr.reveal('.rightSlide-2', {
+    origin: 'right',
+    distance: '50px',
+    duration: 700,
+    delay:0,
+    opacity:1,
+    scale: 1,
+    easing:'ease-in'
+})
+
+sr.reveal('.rightSlide-3', {
+    origin: 'right',
+    distance: '50px',
+    duration: 900,
+    delay:0,
+    opacity:1,
+    scale: 1,
+    easing:'ease-in'
+})
+
